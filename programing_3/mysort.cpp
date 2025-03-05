0bb6525699dbb557eec152a8f0d5d58120187e91
Ben2104 <dohoangkhoi341@gmail.com>
2025-03-05 15:47:21 -0800

    add comments for generate and mysort

--- programing_3/mysort.cpp
+++ programing_3/mysort.cpp
@@ -1,3 +1,10 @@
+// Name: Hoang Do
+// Class (CECS 325-01)
+// Project Name (Prog 3 – Sort Contest)
+// Due Date 03/06/2025
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
 #include <iostream>
 #include <fstream>
 
